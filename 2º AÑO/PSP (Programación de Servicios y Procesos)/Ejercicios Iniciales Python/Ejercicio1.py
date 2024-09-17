0d14a2689858dd5f69cc5722e85f747b5ea447e9
alfonsaco <alfonso.rincondev@gmail.com>
2024-09-17 16:38:47 +0200

    Ejercicio Pyhon de prueba

---
+++ 2º AÑO/PSP (Programación de Servicios y Procesos)/Ejercicios Iniciales Python/Ejercicio1.py
@@ -0,0 +1,15 @@
+import random
+
+print('JUEGO DE DADOS')
+j1=random.randint(0,10)
+j2=random.randint(0,10)
+
+print("J1 TIENE UN {j1}")
+print(f"J2 TIENE UN {j2}")
+
+if(j1 > j2) :
+    print(f"J1 HA GANADO CON UN {j1}")
+elif(j1 == j2) :
+    print("EMPATE")
+else :
+    print(f"J2 HA GANADO CON UN {j2}")
