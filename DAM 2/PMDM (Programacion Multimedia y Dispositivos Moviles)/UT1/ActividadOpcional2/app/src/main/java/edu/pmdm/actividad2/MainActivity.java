141b301a71fe5823fa51539a4ed098fd0318b274
alfonsaco <alfonso.rincondev@gmail.com>
2024-09-26 00:15:43 +0200

    Comenzamos a programar la actividad

--- DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/UT1/ActividadOpcional2/app/src/main/java/edu/pmdm/actividad2/MainActivity.java
+++ DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/UT1/ActividadOpcional2/app/src/main/java/edu/pmdm/actividad2/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button btnResult;
+        TextView txtResult;
+        EditText etxtNumber;
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnResult=findViewById(R.id.btnResult);
+        txtResult=findViewById(R.id.txtResult);
+        etxtNumber=findViewById(R.id.etxtNumber);
+
+        if(!etxtNumber.getText().toString().isEmpty()) {
+            // Evento de pulsar el botón
+            btnResult.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int numero=Integer.parseInt(String.valueOf(etxtNumber.getText()));
+
+                    if(numero == 1) {
+                        txtResult.setText("La posición 1 es del nº primo 1");
+                    } else if(numero == 2) {
+                        txtResult.setText("La posición 2 es del nº primo 1");
+                    }
+
+                }
+            });
+        }
+
     }
 }
