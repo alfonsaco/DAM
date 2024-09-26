c52dbf282a2321eefdba6c53fbe850a55f307caa
alfonsaco <alfonso.rincondev@gmail.com>
2024-09-26 17:04:53 +0200

    Aplicación fútbol terminada

--- DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/UT2/RadioButtonsExample/app/src/main/java/edu/pruebas/radiobuttonsexample/MainActivity.java
+++ DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/UT2/RadioButtonsExample/app/src/main/java/edu/pruebas/radiobuttonsexample/MainActivity.java
@@ -1,24 +1,75 @@
 package edu.pruebas.radiobuttonsexample;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, CheckBox.OnCheckedChangeListener {
+
+    RadioGroup rdGroup;
+    TextView txtGusto;
+    CheckBox CheckBox;
+    TextView txtMensajeEquipo;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        rdGroup=findViewById(R.id.radioGroup);
+        rdGroup.setOnCheckedChangeListener(this);
+        txtGusto=findViewById(R.id.txtGusto);
+        CheckBox=findViewById(R.id.checkBox);
+        CheckBox.setOnCheckedChangeListener(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedInt) {
+        txtMensajeEquipo=findViewById(R.id.txtMensajeEquipo);
+        imageView=findViewById(R.id.imageView);
+
+        if(checkedInt == R.id.rbTalavera) {
+            txtMensajeEquipo.setText("Buena elección, ¡El Talavera promete!");
+            imageView.setImageResource(R.drawable.talavera);
+        } else if(checkedInt == R.id.rbAlcazar) {
+            txtMensajeEquipo.setText("¡Gran equipo la Gimnástica!");
+            imageView.setImageResource(R.drawable.alcazar);
+        } else if(checkedInt == R.id.rbAlbacete) {
+            txtMensajeEquipo.setText("El Albacete no es el mismo desde que se fue Iniesta");
+            imageView.setImageResource(R.drawable.albacete);
+        } else {
+            txtMensajeEquipo.setText("El dinero no lo es todo...");
+            imageView.setImageResource(R.drawable.otros);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        txtGusto=findViewById(R.id.txtGusto);
+
+        if(b == true) {
+            txtGusto.setText("Te gusta el fútbol");
+        } else {
+            txtGusto.setText("No te gusta el fútbol");
+        }
     }
 }
