388a90107627831fb4c370104376b072865e8b0e
alfonsaco <alfonso.rincondev@gmail.com>
2024-10-17 15:39:36 +0200

    Ejercicio Listeners DI

---
+++ DAM 2/DI (Desarrollo de Interfaces)/UT3/pruebaEventos/src/pruebaeventos/MiActionListener.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaeventos;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author PROGRAMACION
+ */
+public class MiActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        System.out.println("Se pulsó el botón");
+    }
+    
+}
