450a7396cfe27e551e6ad60ca4c43e36c64be2ea
alfonsaco <alfonso.rincondev@gmail.com>
2024-11-11 12:03:57 +0100

    Grid en proceso

--- DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/Practicas/PRC2_AlfonsoRincon/app/src/main/java/edu/pruebas/prc2_alfonsorincon/BoardActivity.java
+++ DAM 2/PMDM (Programacion Multimedia y Dispositivos Moviles)/Practicas/PRC2_AlfonsoRincon/app/src/main/java/edu/pruebas/prc2_alfonsorincon/BoardActivity.java
@@ -1,6 +1,10 @@
 package edu.pruebas.prc2_alfonsorincon;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BoardActivity extends AppCompatActivity {
+
+    // Array de los botones. Como algunos son ImageButtons, y otro Buttons, no se pueden
+    // almacenar en un array de Buttons, por ejemplo, por lo que los almaceno en un Array de
+    // View, ya que ambos heredan de esta clase.
+    private View[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class BoardActivity extends AppCompatActivity {
             case "easy":
                 int[][] board=locateMines(8,8,10);
                 printBoard(board);
-                createGrid(board);
+                createGrid(board ,8);
                 break;
 
             case "medium":
@@ -50,7 +59,74 @@ public class BoardActivity extends AppCompatActivity {
     }
 
     // Método para crear el grid mediante el array de números
-    private void createGrid(int[][] board) {
+    private void createGrid(int[][] board, int gridSize) {
+        GridLayout gridLayout=findViewById(R.id.gridLayout);
+        gridLayout.setRowCount(gridSize);
+        gridLayout.setRowCount(gridSize);
+
+        // Definir el tamaño del botón
+        //      getDisplayMetrics(): contiene información sobre la pantalla del dispositivo
+        //      widthPixels: devuelve el ancho en píxeles
+        int buttonSize=getResources().getDisplayMetrics().widthPixels / gridSize;
+        buttons=new View[gridSize][gridSize];
+
+        for (int i=0; i<board.length; i++) {
+            for (int e=0; e<board[i].length; e++) {
+                if(board[i][e] == -1) {
+                    ImageButton button=new ImageButton(this);
+
+                    // Configuración básica para ambos tipos de botón
+                    button.setLayoutParams(new GridLayout.LayoutParams());
+                    button.getLayoutParams().width = buttonSize;
+                    button.getLayoutParams().height = buttonSize;
+
+                    // Guardar el botón en el array de View
+                    buttons[i][e] = button;
+
+                    // Configurar comportamiento al hacer clic
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Acción al hacer clic en la casilla
+                            if (v instanceof ImageButton) {
+                                // Acciones para la mina
+                            } else if (v instanceof Button) {
+                                // Acciones para una casilla normal
+                            }
+                        }
+                    });
+
+                    // Añadir el botón al GridLayout
+                    gridLayout.addView(button);
+                } else {
+                    Button button=new Button(this);
+
+                    // Configuración básica para ambos tipos de botón
+                    button.setLayoutParams(new GridLayout.LayoutParams());
+                    button.getLayoutParams().width = buttonSize;
+                    button.getLayoutParams().height = buttonSize;
+
+                    // Guardar el botón en el array de View
+                    buttons[i][e] = button;
+
+                    // Configurar comportamiento al hacer clic
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Acción al hacer clic en la casilla
+                            if (v instanceof ImageButton) {
+                                // Acciones para la mina
+                            } else if (v instanceof Button) {
+                                // Acciones para una casilla normal
+                            }
+                        }
+                    });
+
+                    // Añadir el botón al GridLayout
+                    gridLayout.addView(button);
+                }
+            }
+        }
 
     }
 
