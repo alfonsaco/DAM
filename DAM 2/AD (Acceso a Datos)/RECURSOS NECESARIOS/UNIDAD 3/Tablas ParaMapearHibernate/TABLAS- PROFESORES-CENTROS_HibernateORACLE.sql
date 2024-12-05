21dba0e33efb30f31489a0195862a5184e92a6cc
alfonsaco <alfonso.rincondev@gmail.com>
2024-12-05 17:30:43 +0100

    Material de la carpeta compartida

---
+++ DAM 2/AD (Acceso a Datos)/RECURSOS NECESARIOS/UNIDAD 3/Tablas ParaMapearHibernate/TABLAS- PROFESORES-CENTROS_HibernateORACLE.sql
@@ -0,0 +1,270 @@
+-- ******************************************************
+-- ****** C1_CENTROS, C1_PROFESORES, C1_ESPECIALIDAD, C1_ASIGNATURAS, C1_ASIGPROF
+-- *******************************************************
+
+DROP TABLE C1_ASIGPROF cascade constraints;
+DROP TABLE C1_PROFESORES cascade constraints; 
+DROP TABLE C1_CENTROS cascade constraints;
+DROP TABLE C1_ESPECIALIDAD cascade constraints;
+DROP TABLE C1_ASIGNATURAS cascade constraints;
+
+
+-- ***************************** creacion tablas DATA MODELER
+
+
+
+CREATE TABLE C1_ASIGNATURAS 
+    ( 
+     COD_ASIG CHAR (6 BYTE)  NOT NULL , 
+     NOMBRE_ASI VARCHAR2 (30 BYTE) 
+    ) ;
+
+
+
+ALTER TABLE C1_ASIGNATURAS 
+    ADD CONSTRAINT C1_ASIGNATURAS_PK PRIMARY KEY ( COD_ASIG ) ;
+
+
+
+CREATE TABLE C1_ASIGPROF 
+    ( 
+         C1_ASIGNATURAS_COD_ASIG CHAR (6 BYTE)  NOT NULL ,
+         C1_PROFESORES_COD_PROF NUMBER (4)  NOT NULL
+
+    ) ;
+
+
+
+ALTER TABLE C1_ASIGPROF 
+    ADD CONSTRAINT C1_ASIGPROF__IDX PRIMARY KEY ( C1_PROFESORES_COD_PROF, C1_ASIGNATURAS_COD_ASIG ) ;
+
+
+
+CREATE TABLE C1_CENTROS 
+    ( 
+     COD_CENTRO NUMBER (4)  NOT NULL , 
+     NOM_CENTRO VARCHAR2 (20 BYTE) , 
+     DIRECTOR NUMBER (4) , 
+     DIRECCION VARCHAR2 (25 BYTE) , 
+     LOCALIDAD VARCHAR2 (20 BYTE) , 
+     PROVINCIA VARCHAR2 (20 BYTE) 
+    ) ;
+
+
+
+ALTER TABLE C1_CENTROS 
+    ADD CONSTRAINT C1_CENTROS_PK PRIMARY KEY ( COD_CENTRO ) ;
+
+
+
+CREATE TABLE C1_ESPECIALIDAD 
+    ( 
+     ESPECIALIDAD CHAR (2 BYTE)  NOT NULL , 
+     NOMBRE_ESPE VARCHAR2 (30 BYTE) 
+    ) ;
+
+
+
+ALTER TABLE C1_ESPECIALIDAD 
+    ADD CONSTRAINT C1_ESPECIALIDAD_PK PRIMARY KEY ( ESPECIALIDAD ) ;
+
+
+
+CREATE TABLE C1_PROFESORES 
+    ( 
+     COD_PROF NUMBER (4)  NOT NULL , 
+     NOMBRE_APE VARCHAR2 (30 BYTE) , 
+     ESPECIALIDAD CHAR (2 BYTE) , 
+     COD_PROF1 NUMBER (4),
+     FECHA_NAC DATE , 
+     SEXO CHAR (1 BYTE) ,   
+     COD_CENTRO NUMBER (4) 
+    );
+
+
+
+ALTER TABLE C1_PROFESORES 
+    ADD CONSTRAINT C1_PROFESORES_PK PRIMARY KEY ( COD_PROF ) ;
+
+
+
+
+ALTER TABLE C1_PROFESORES 
+    ADD CONSTRAINT Centros_prof FOREIGN KEY 
+    ( 
+     COD_CENTRO
+    ) 
+    REFERENCES C1_CENTROS 
+    ( 
+     COD_CENTRO
+    ) ;
+
+
+ALTER TABLE C1_ASIGPROF 
+    ADD CONSTRAINT FK_ASS1_5 FOREIGN KEY 
+    ( 
+     C1_PROFESORES_COD_PROF
+    ) 
+    REFERENCES C1_PROFESORES 
+    ( 
+     COD_PROF
+    ) 
+    ON DELETE CASCADE 
+;
+
+
+ALTER TABLE C1_ASIGPROF 
+    ADD CONSTRAINT FK_ASS2_6 FOREIGN KEY 
+    ( 
+     C1_ASIGNATURAS_COD_ASIG
+    ) 
+    REFERENCES C1_ASIGNATURAS 
+    ( 
+     COD_ASIG
+    ) 
+    ON DELETE CASCADE 
+;
+
+
+ALTER TABLE C1_PROFESORES 
+    ADD CONSTRAINT espeprofes FOREIGN KEY 
+    ( 
+     ESPECIALIDAD
+    ) 
+    REFERENCES C1_ESPECIALIDAD 
+    ( 
+     ESPECIALIDAD
+    ) 
+;
+
+
+
+
+
+
+-- *********************************** TABLA C1_ESPECIALIDAD
+
+insert into C1_ESPECIALIDAD VALUES ('IF','Informática');
+insert into C1_ESPECIALIDAD VALUES ('IN','Inglés');
+insert into C1_ESPECIALIDAD VALUES ('FQ','Física y Química');
+insert into C1_ESPECIALIDAD VALUES ('GH','Geografía e Historia');
+insert into C1_ESPECIALIDAD VALUES ('TG','Tecnología');
+insert into C1_ESPECIALIDAD VALUES ('LG','Lengua');
+insert into C1_ESPECIALIDAD VALUES ('DB','Dibujo');
+insert into C1_ESPECIALIDAD VALUES ('MT','Matemáticas');
+
+-- *********************************** TABLA C1_CENTROS
+
+INSERT INTO C1_CENTROS VALUES (1000,'IES El Quijote', 1000,
+'Avda. Los Molinos 25', 'GUADALAJARA', 'GUADALAJARA');
+
+INSERT INTO C1_CENTROS VALUES (1015,'CP Los Danzantes', 1010,
+'C/Las Musas s/n','PASTRANA', 'GUADALAJARA');
+
+INSERT INTO C1_CENTROS VALUES (1022, 'IES Planeta Tierra',2000, 
+ 'C/Mina 45', 'AZUQUECA', 'GUADALAJARA');
+
+INSERT INTO C1_CENTROS VALUES (1045, 'CP Manuel Hidalgo', NULL, 
+  'C/Granada 5', 'GUADALAJARA', 'GUADALAJARA');
+
+INSERT INTO C1_CENTROS VALUES (1050, 'IES Antoñete', NULL,
+  'C/Los Toreros 21', 'SIGUENZA', 'GUADALAJARA');
+COMMIT;
+
+-- *********************************** TABLA C1_PROFESORES
+
+
+INSERT INTO C1_PROFESORES VALUES (1000, 
+'Martínez Salas, Fernando', 'IF', 1001, '07-09-1961', 'H', 1000);
+
+INSERT INTO C1_PROFESORES VALUES (1001, 
+'Bueno Zarco, Elisa', 'IF',NULL, '17-02-1960', 'M', 1000);
+
+INSERT INTO C1_PROFESORES VALUES (2002, 
+'Rivera Silvestre, Ana','DB',3000, '10-10-1950', 'M',1000);
+
+INSERT INTO C1_PROFESORES VALUES (3000,  
+'De Lucas Fdez, M.Angel','DB',NULL, '09-09-1980','M',1000);
+
+
+INSERT INTO C1_PROFESORES VALUES (1010, 
+'Montes García, M.Pilar', 'MT', 1011,'10-10-1970', 'M', 1015);
+
+INSERT INTO C1_PROFESORES VALUES (1011, 
+'Arroba Conde, Manuel', 'MT', NULL,'10-12-1970', 'H', 1015);
+
+INSERT INTO C1_PROFESORES VALUES (1022, 
+'Ruiz Lafuente, Manuel','MT',1011, '11-11-1966', 'H',1015);
+
+
+INSERT INTO C1_PROFESORES VALUES (2000, 
+'Ramos Ruiz, Luis','LG',2003, '08-08-1963', 'H',1022 );
+
+INSERT INTO C1_PROFESORES VALUES (2003, 
+'Segura Molina, Irene','LG',NULL, '08-07-1963', 'M',1022 );
+
+INSERT INTO C1_PROFESORES VALUES (1045, 
+'Serrano Laguía, María','IF',NULL,'01-02-1976', 'M', 1022);
+
+COMMIT;
+
+-- Añadimos clave ajena, primero insertamos y luego añadimos la clave ajena
+-- así evitamos algun error de los insert.
+
+ALTER TABLE C1_PROFESORES 
+    ADD CONSTRAINT jefedep FOREIGN KEY 
+    ( 
+     COD_PROF1
+    ) 
+    REFERENCES C1_PROFESORES 
+    ( 
+     COD_PROF
+    ) ;
+
+-- *********************************** TABLA C1_ASIGNATURAS
+
+
+insert into C1_ASIGNATURAS VALUES ('IF0001','DAHC');
+insert into C1_ASIGNATURAS VALUES ('IF0002','RAL');
+insert into C1_ASIGNATURAS VALUES ('IF0003','IMSI');
+insert into C1_ASIGNATURAS VALUES ('IF0004','DPEG');
+insert into C1_ASIGNATURAS VALUES ('IF0006','PLE');
+insert into C1_ASIGNATURAS VALUES ('IF0007','FPE');
+
+insert into C1_ASIGNATURAS VALUES ('LG0001','Lengua 1 ESO');
+insert into C1_ASIGNATURAS VALUES ('LG0002','Lengua 2 ESO');
+insert into C1_ASIGNATURAS VALUES ('LG0003','Lengua 3 ESO');
+insert into C1_ASIGNATURAS VALUES ('LG0004','Lengua 4 ESO');
+
+insert into C1_ASIGNATURAS VALUES ('DB0001','Plástica');
+insert into C1_ASIGNATURAS VALUES ('DB0002','Taller cerámica');
+insert into C1_ASIGNATURAS VALUES ('DB0003','Dibujo Técnico');
+
+insert into C1_ASIGNATURAS VALUES ('MT0001','Matemáticas 1 BAC');
+insert into C1_ASIGNATURAS VALUES ('MT0002','Matemáticas 2 BAC');
+
+-- *********************************** TABLA C1_ASIGPROF
+
+insert into C1_ASIGPROF VALUES ('IF0002',1001);
+insert into C1_ASIGPROF VALUES ('IF0003',1001);
+insert into C1_ASIGPROF VALUES ('IF0001',1000);
+
+insert into C1_ASIGPROF VALUES ('LG0001',2000);
+insert into C1_ASIGPROF VALUES ('LG0002',2000);
+insert into C1_ASIGPROF VALUES ('LG0003',2003);
+insert into C1_ASIGPROF VALUES ('LG0004',2003);
+
+insert into C1_ASIGPROF VALUES ('DB0001',2002);
+insert into C1_ASIGPROF VALUES ('DB0002',2002);
+insert into C1_ASIGPROF VALUES ('DB0003',3000);
+
+insert into C1_ASIGPROF VALUES ('MT0001',1010);
+insert into C1_ASIGPROF VALUES ('MT0001',1011);
+insert into C1_ASIGPROF VALUES ('MT0001',1022);
+insert into C1_ASIGPROF VALUES ('MT0002',1010);
+
+COMMIT;
+
+
+
+
---
+++ DAM 2/AD (Acceso a Datos)/RECURSOS NECESARIOS/UNIDAD 3/Tablas ParaMapearHibernate/Tablas_Metro_Oracle.sql
@@ -0,0 +1,262 @@
+
+
+PURGE RECYCLEBIN;
+
+
+DROP TABLE T_LINEAS CASCADE CONSTRAINTS;
+create table  T_LINEAS(
+ cod_linea      NUMBER(5) primary key,
+ nombre          varchar(50) not null
+ ) ;
+
+Insert into  T_LINEAS  values(1,'PLAZA CASTILLA-CONGOSTO');
+Insert into  T_LINEAS  values(2,'VENTAS-CUATRO CAMINOS');
+Insert into  T_LINEAS  values(3,'LEGAZPI-MONCLOA');
+Insert into  T_LINEAS  values(4,'ARGÜELLES-PARQUE DE SANTA MARÍA');
+Insert into  T_LINEAS  values(5,'CANILLEJAS-CASA DE CAMPO');
+Insert into  T_LINEAS  values(6,'CIRCULAR');
+Insert into  T_LINEAS  values(7,'LAS MUSAS-PITIS');
+
+
+DROP TABLE T_ESTACIONES CASCADE CONSTRAINTS;
+create table  T_estaciones(
+ cod_estacion    NUMBER(5) primary key,
+ nombre           varchar(50) not null,
+ direccion        varchar(50) not null
+ );
+
+Insert into  T_estaciones  values(1,'PLAZA DE CASTILLA','PLAZA DE CASTILLA');
+Insert into  T_estaciones  values(2,'VALDEACEDERAS','BARRIO VALDEACEDERAS');
+Insert into  T_estaciones  values(3,'TETUÁN','BARUO DE TETUÁN');
+Insert into  T_estaciones  values(4,'ALVARADO','CALLE ALVARADO');
+Insert into  T_estaciones  values(5,'CUATRO CAMINOS','AVDA CUATRO CAMINOS');
+Insert into  T_estaciones  values(6,'IGLESIA','RONDA DE GOYA' );
+Insert into  T_estaciones  values(7,'GRAN VÍA','C/GRAN VÍA');
+Insert into  T_estaciones  values(8,'SOL','PUERTA DEL SOL');
+Insert into  T_estaciones  values(9,'ANTON MARTÍN','C/ ANTÓN MARTÍN');
+Insert into  T_estaciones  values(10,'ATOCHA','CALLE ATOCHA');
+Insert into  T_estaciones  values(11,'PUENTE DE VALLECAS','VALLECAS');
+Insert into  T_estaciones  values(12,'PORTAZGO','C/LOS COMUNEROS');
+Insert into  T_estaciones  values(13,'CONGOSTO','C/SIERRA DE BOBIA');
+
+Insert into  T_estaciones  values(14,'VENTAS','PLAZA DE VENTAS');
+Insert into  T_estaciones  values(15,'GOYA','CALLE GOYA');
+Insert into  T_estaciones  values(16,'SEVILLA','PLAZA DE SEVILLA');
+Insert into  T_estaciones  values(17,'ÓPERA','PLAZA DE ÓPERA');
+Insert into  T_estaciones  values(18,'SAN BERNARDO','C/ SAN BERNARDO');
+Insert into  T_estaciones  values(19,'QUEVEDO','GLORIETA DE QUEVEDO');
+
+Insert into  T_estaciones  values(20,'LEGAZPI','PLAZA DE LEGAZPI');
+Insert into  T_estaciones  values(21,'DELICIAS','CALLE DELICIAS');
+Insert into  T_estaciones  values(22,'CALLAO','PLAZA DE CALLAO');
+Insert into  T_estaciones  values(23,'VENTURA RODRIGUEZ','C/ VENTURA 20');
+Insert into  T_estaciones  values(24,'ARGÜELLES','PLAZA DE ARGÜELLES');
+Insert into  T_estaciones  values(25,'MONCLOA','PALACIO DE LA MONCLOA');
+
+Insert into  T_estaciones  values(26,'BILBAO','C/ BILBAO');
+Insert into  T_estaciones  values(27,'ALONSO MARTÍNEZ','C/ ALONSO MARTINEZ');
+Insert into  T_estaciones  values(28,'COLÓN','PLAZA DE COLÓN');
+Insert into  T_estaciones  values(29,'LISTA','C/ LISTA DE AMÉRICA');
+Insert into  T_estaciones  values(30,'AVENIDA DE AMÉRICA','AVDA DE AMÉRICA 10');
+Insert into  T_estaciones  values(31,'CANILLAS','PLAZA DE CANILLAS');
+Insert into  T_estaciones  values(32,'PARQUE DE SANTA MARÍA','BARRIO DE SANTA MARÍA');
+
+DROP TABLE T_LINEA_ESTACION CASCADE CONSTRAINTS;
+create table  T_linea_estacion(
+ cod_linea     NUMBER(5)  not null,
+ cod_estacion  NUMBER(5)  not null,
+ orden         NUMBER(5)  not null,
+ FOREIGN KEY (cod_linea)    REFERENCES   T_LINEAS(cod_linea),
+ FOREIGN KEY (cod_estacion) REFERENCES   T_estaciones(cod_estacion),
+ PRIMARY KEY (cod_linea, cod_estacion)
+ ) ;
+
+Insert into  T_linea_estacion values(1,1,1);
+Insert into  T_linea_estacion values(1,2,2);
+Insert into  T_linea_estacion values(1,3,3);
+Insert into  T_linea_estacion values(1,4,4);
+Insert into  T_linea_estacion values(1,5,5);
+Insert into  T_linea_estacion values(1,6,6);
+Insert into  T_linea_estacion values(1,7,7);
+Insert into  T_linea_estacion values(1,8,8);
+Insert into  T_linea_estacion values(1,9,9);
+Insert into  T_linea_estacion values(1,10,10);
+Insert into  T_linea_estacion values(1,11,11);
+Insert into  T_linea_estacion values(1,12,12);
+Insert into  T_linea_estacion values(1,13,13);
+
+Insert into  T_linea_estacion values(2,14,1);
+Insert into  T_linea_estacion values(2,15,2);
+Insert into  T_linea_estacion values(2,16,3);
+Insert into  T_linea_estacion values(2,8,4);
+Insert into  T_linea_estacion values(2,17,5);
+Insert into  T_linea_estacion values(2,18,6);
+Insert into  T_linea_estacion values(2,19,7);
+Insert into  T_linea_estacion values(2,5,8);
+
+Insert into  T_linea_estacion values(3,20,1);
+Insert into  T_linea_estacion values(3,21,2);
+Insert into  T_linea_estacion values(3,8,3);
+Insert into  T_linea_estacion values(3,22,4);
+Insert into  T_linea_estacion values(3,23,5);
+Insert into  T_linea_estacion values(3,24,6);
+Insert into  T_linea_estacion values(3,25,7);
+
+Insert into  T_linea_estacion values(4,24,1);
+Insert into  T_linea_estacion values(4,18,2);
+Insert into  T_linea_estacion values(4,26,3);
+Insert into  T_linea_estacion values(4,27,4);
+Insert into  T_linea_estacion values(4,28,5);
+Insert into  T_linea_estacion values(4,15,6);
+Insert into  T_linea_estacion values(4,29,7);
+Insert into  T_linea_estacion values(4,30,8);
+Insert into  T_linea_estacion values(4,31,9);
+Insert into  T_linea_estacion values(4,32,10);
+
+
+DROP TABLE T_ACCESOS CASCADE CONSTRAINTS;
+create table  T_accesos(
+ cod_acceso     NUMBER(5)  primary key,
+ descripcion    varchar(50) not null,
+ cod_estacion   NUMBER(5)  not null,
+FOREIGN KEY (cod_estacion) REFERENCES   T_estaciones(cod_estacion)
+) ;
+
+
+Insert into  T_accesos values(1,'BULEVAR DE LA CASTELLANA',1);
+Insert into  T_accesos values(2,'CASTELLANA 189',1);
+Insert into  T_accesos values(3,'BRAVO MURILLO 377',1);
+
+Insert into  T_accesos values(4,'CAPITÁN BLANCO 20',2);
+Insert into  T_accesos values(5,'BRAVO MURILLO 324',2);
+Insert into  T_accesos values(6,'ANIBAL 1',2);
+
+Insert into  T_accesos values(7,'ALGODONALES',3);
+Insert into  T_accesos values(8,'GENERAL MARGALLO',3);
+
+Insert into  T_accesos values(9,'BRAVO MURILLO 135',4);
+Insert into  T_accesos values(10,'BRAVO MURILLO 136',4);
+
+Insert into  T_accesos values(11,'BRAVO MURILLO 101',5);
+Insert into  T_accesos values(12,'REINA VICTORIA 1',5);
+Insert into  T_accesos values(13,'SANTA ENGRACIA 168',5);
+Insert into  T_accesos values(14,'MAUDES 8',5);
+
+Insert into  T_accesos values(15,'ELOY GONZALO 1',6);
+Insert into  T_accesos values(16,'GENERAL MARTÍNEZ CAMPOS 38',6);
+Insert into  T_accesos values(17,'SANTA ENGRACIA 58',6);
+
+Insert into  T_accesos values(18,'MONTERA 44',7);
+Insert into  T_accesos values(19,'GRAN VÍA 25',7);
+Insert into  T_accesos values(20,'GRAN VÍA 26',7);
+
+Insert into  T_accesos values(21,'CARRETAS',8);
+Insert into  T_accesos values(22,'CARMEN',8);
+Insert into  T_accesos values(23,'ALCALÁ 12',8);
+Insert into  T_accesos values(24,'PRECIADOS 3',8);
+
+Insert into  T_accesos values(25,'CARRETAS',9);
+Insert into  T_accesos values(26,'CARMEN',9);
+Insert into  T_accesos values(27,'ALCALÁ 39',9);
+
+Insert into  T_accesos values(28,'AMOR DE DIOS 14',10);
+Insert into  T_accesos values(29,'ATOCHA 83',10);
+Insert into  T_accesos values(30,'ATOCHA 76',10);
+
+Insert into  T_accesos values(31,'ALBUFERA 14',11);
+Insert into  T_accesos values(32,'ALBUFERA 9',11);
+
+Insert into  T_accesos values(33,'ALBUFERA 108',12); 
+Insert into  T_accesos values(34,'ALBUFERA 117',12);
+Insert into  T_accesos values(35,'TENIENTE MUÑOZ DÍAZ',12);
+
+Insert into  T_accesos values(36,'CONGOSTO 35',13); 
+Insert into  T_accesos values(37,'PLAZA DE CONGOSTO 33',13);
+
+Insert into  T_accesos values(38,'ALCALÁ 233',14); 
+Insert into  T_accesos values(39,'PLAZA DE TOROS 1',14);
+
+Insert into  T_accesos values(40,'CONDE DE PEÑALVER 2',15); 
+Insert into  T_accesos values(41,'FELIPE II 1',15);
+Insert into  T_accesos values(42,'GENERAL DIEZ PORLIER',15);
+Insert into  T_accesos values(43,'FELIPE II 12',15);
+
+Insert into  T_accesos values(44,'SEVILLA',16); 
+Insert into  T_accesos values(45,'VIRGEN DE LOS PELIGROS',16);
+Insert into  T_accesos values(46,'IGLESIA DE CALATRAVA',16);
+
+Insert into  T_accesos values(47,'ARENAL.Plaza de Isabel II, 1',17); 
+
+Insert into  T_accesos values(48,'ALBERTO AGUILERA',18); 
+Insert into  T_accesos values(49,'CARRANZA',18);
+
+
+Insert into  T_accesos values(50,'ARAPILES. GLTA QUEVEDO 2',19); 
+Insert into  T_accesos values(51,'FUENCARRAL. GLTA QUEVEDO 1',19);
+
+
+DROP TABLE T_COCHERAS CASCADE CONSTRAINTS;
+create table  T_cocheras(
+ cod_cochera    NUMBER(5) primary key,
+ nombre         varchar(50) not null,
+ direccion      varchar(50) not null
+ ) ;
+
+Insert into  T_cocheras values(1,'COCHERA ARGÜELLES','MADRID. CENTRO ARGÜELLES S/N'); 
+Insert into  T_cocheras values(2,'COCHERA MONCLOA','MADRID. AVDA MONCLOA S/N'); 
+Insert into  T_cocheras values(3,'COCHERA MIGUEL HERNÁNDEZ','MADRID. BARRIO DEL PILAR'); 
+Insert into  T_cocheras values(4,'COCHERA CIUDAD UNIVERSITARIA','MADRID. CIUDAD UNIVERSITARIA 100'); 
+Insert into  T_cocheras values(5,'COCHERA NUEVOS MINISTERIOS','MADRID. NUEVOS MINISTERIOS 200'); 
+Insert into  T_cocheras values(6,'COCHERA UNIVERSIDAD REY JUAN CARLOS','LEGANES. CIUDAD INIVERSITARIA'); 
+Insert into  T_cocheras values(7, 'COCHERAS PARA METRO LIGERO DE BOADILLA Y POZUELO','BOADILLA DEL MONTE'); 
+Insert into  T_cocheras values(8, 'DEPÓSITO CUATRO CAMINOS','MADRID. CUATRO VIENTOS'); 
+Insert into  T_cocheras values(9, 'DEPÓSITO LAS VENTAS','MADRID. LAS VENTAS 100'); 
+Insert into  T_cocheras values(10, 'DEPÓSITO PLAZA DE CASTILLA','MADRID. PLAZA DE CASTILLA'); 
+Insert into  T_cocheras values(11, 'DEPÓSITO FUENCARRAL','MADRID. BARRIO FUENCARRAL'); 
+Insert into  T_cocheras values(12, 'DEPÓSITO CANILLEJAS','MADRID. BARRIO CANILLEJAS'); 
+Insert into  T_cocheras values(13, 'DEPÓSITO VALDECARROS','MADRID. EL ENSANCHE DE VALLECAS'); 
+
+
+
+DROP TABLE T_TRENES CASCADE CONSTRAINTS;
+create table  T_trenes(
+ cod_tren    NUMBER(5) primary key,
+ nombre      varchar(50) not null,
+ tipo       varchar(20) not null,
+ cod_linea     NUMBER(5) not null,
+ cod_cochera   NUMBER(5)  not null,
+ FOREIGN KEY (cod_linea)    REFERENCES   T_LINEAS(cod_linea),
+ FOREIGN KEY (cod_cochera) REFERENCES   T_cocheras(cod_cochera)
+) ;
+
+Insert into  t_trenes values (1,'COCHE 3000 , 10','SERIE 3000',2,1);
+Insert into  t_trenes values (2,'COCHE 3000 , 20','SERIE 3000',2,1);
+Insert into  t_trenes values (3,'COCHE 3000 , 30','SERIE 3000',2,1);
+
+Insert into  t_trenes values (4,'COCHE 5000 , 1','SERIE 5000',1,2);
+Insert into  t_trenes values (5,'COCHE 5000 , 2','SERIE 5000',1,2);
+Insert into  t_trenes values (6,'COCHE 5000 , 3','SERIE 6000',1,2);
+Insert into  t_trenes values (7,'COCHE 5000 , 4','SERIE 5000',1,2);
+
+Insert into  t_trenes values (8,'COCHE 6000 , 10','SERIE 3000',3,3);
+Insert into  t_trenes values (9,'COCHE 6000 , 20','SERIE 3000',3,3);
+Insert into  t_trenes values (10,'COCHE 6000 , 30','SERIE 6000',3,3);
+
+Insert into  t_trenes values (11,'COCHE 9000 , 1','SERIE 9000',4,2);
+Insert into  t_trenes values (12,'COCHE 9000 , 2','SERIE 9000',4,2);
+Insert into  t_trenes values (13,'COCHE 9000 , 3','SERIE 9000',4,2);
+Insert into  t_trenes values (14,'COCHE 9000 , 4','SERIE 9000',4,2);
+
+
+Insert into  t_trenes values (15,'COCHE 8000 , 11','SERIE 8000',5,6);
+Insert into  t_trenes values (16,'COCHE 8000 , 12','SERIE 8000',5,6);
+Insert into  t_trenes values (17,'COCHE 8000 , 13','SERIE 3000',5,6);
+
+Insert into  t_trenes values (18,'COCHE 8400 , 41','SERIE 8400',6,7);
+Insert into  t_trenes values (19,'COCHE 8400 , 42','SERIE 8400',6,7);
+Insert into  t_trenes values (20,'COCHE 8400 , 43','SERIE 8400',6,7);
+
+Insert into  t_trenes values (21,'COCHE 8400 , 54','SERIE 3000',7,5);
+Insert into  t_trenes values (22,'COCHE 8400 , 55','SERIE 8400',7,5);
+
+COMMIT;
